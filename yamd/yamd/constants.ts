6854bf4756dee3c3d0f6ca82d8a2664f3f458833
Li Xuanji <xuanji@gmail.com>
2015-12-07 22:35:05 -0500

    state, reg, finish decode

---
+++ yamd/yamd/constants.ts
@@ -0,0 +1,46 @@
+﻿export let Reg = {
+        'savedPC': 31,
+        'stackPointer': 30,
+        'framePointer': 29,
+        'heapPointer': 28,
+        'semiSpaceTop': 27,
+        'input1': 1,
+        'input2': 2,
+        'result': 3,
+        'scratch': 4,
+        'savedParamPtr': 5,
+        'allocated': 6,
+        'copyChunkScratch': 7,
+        'targetPC': 8,
+        'scratchPtrForGC': 9
+}
+
+/** The PC value at which the CPU should halt execution. */
+export let terminationPC = "11111110111000011101111010101101";
+
+/** The address that is one word beyond the last valid memory address. */
+export let maxAddr = "00000001000000000000000000000000";
+
+/** The special address for producing output. When a word is stored to this address, the low-order 8 bits of
+    * that word are written to standard output.
+    */
+export let printAddr = "11111111111111110000000000001100";
+
+export interface State {
+    PC: string;
+    LO: string;
+    HI: string;
+    registers: string[];
+    memory: { [n: number]:string };
+}
+
+var initialState: State = {
+    PC: "00000000000000000000000000000000",
+    LO: "00000000000000000000000000000000",
+    HI: "00000000000000000000000000000000",
+    registers: [],
+    memory: {}
+}
+
+initialState.registers[30] = "00000001000000000000000000000000";
+initialState.registers[31] = "11111110111000011101111010101101";
--- yamd/yamd/cpu.ts
+++ yamd/yamd/cpu.ts
@@ -1,8 +1,16 @@
 ﻿/// <reference path="node.d.ts" />
 
 import fs = require('fs');
+import reg = require('./constants');
 
-let decode = function (instruction: number, prev_is_lis = false): string {
+let zeroPad = function (num: number, places: number = 32): string {
+    var zero = places - num.toString().length + 1;
+    return Array(+(zero > 0 && zero)).join("0") + num;
+}
+
+let step = function (state: reg.State): reg.State {
+    
+    let instruction: number = parseInt(state.memory[state.PC], 2);
 
     let d = (instruction >> 11) & 0b11111
     let s = (instruction >> 21) & 0b11111
@@ -14,87 +22,91 @@ let decode = function (instruction: number, prev_is_lis = false): string {
         i -= 0x10000
     }
 
-    if (prev_is_lis) {
-        return instruction.toString(); // return numerical value of inst
-    }
+    state.PC = zeroPad(parseInt(state.PC, 2) + 4);
 
     if ((instruction & 0b11111100000000000000011111111111) >>> 0 == 0b00000000000000000000000000100000) {
-        return `add $${d} $${s} $${t}`
+        // ADD
+        let r = state.registers;
+        state.registers[d] = zeroPad(parseInt(r[s], 2) + parseInt(r[t], 2));
+        return state;
     }
     else if ((instruction & 0b11111100000000000000011111111111) >>> 0 === 0b00000000000000000000000000100010) {
-        return `sub $${d} $${s} $${t}`
+        // SUB
+        let r = state.registers;
+        state.registers[d] = zeroPad(parseInt(r[s], 2) - parseInt(r[t], 2));
+        return state;
     }
     else if ((instruction & 0b11111100000000001111111111111111) >>> 0 === 0b00000000000000000000000000011000) {
-        return `mult $${s}} $${t}`
+        // return `mult $${s}} $${t}`
+        return state;
     }
     else if ((instruction & 0b11111100000000001111111111111111) >>> 0 === 0b00000000000000000000000000011001) {
-        return `multu $${s}} $${t}`
+        // return `multu $${s}} $${t}`
+        return state;
     }
     else if ((instruction & 0b11111100000000001111111111111111) >>> 0 === 0b00000000000000000000000000011010) {
-        return `div $${s}} $${t}`
+        // return `div $${s}} $${t}`
+        return state;
     }
     else if ((instruction & 0b11111100000000001111111111111111) >>> 0 === 0b00000000000000000000000000011011) {
-        return `divu $${s}} $${t}`
+        // return `divu $${s}} $${t}`
+        return state;
     }
     else if ((instruction & 0b11111111111111110000011111111111) >>> 0 === 0b00000000000000000000000000010000) {
-        return `mfhi $${d}}`
+        // return `mfhi $${d}}`
+        return state;
     }
     else if ((instruction & 0b11111111111111110000011111111111) >>> 0 === 0b00000000000000000000000000010010) {
-        return `mflo $${d}}`
+        // return `mflo $${d}}`
+        return state;
     }
     else if ((instruction & 0b11111111111111110000011111111111) >>> 0 === 0b00000000000000000000000000010100) {
-        return `lis $${d}}`
+        // LIS
+        state.registers[d] = state.memory[parseInt(state.PC, 2)]
+        return state;
     }
     else if ((instruction & 0b11111100000000000000000000000000) >>> 0 === 0b10001100000000000000000000000000) {
-        return `lw $${t}, ${i}($${s})`
+        // return `lw $${t}, ${i}($${s})`
     }
     else if ((instruction & 0b11111100000000000000000000000000) >>> 0 === 0b10101100000000000000000000000000) {
-        return `sw $${t}, ${i}($${s})`
+        // return `sw $${t}, ${i}($${s})`
     }
     else if ((instruction & 0b11111100000000000000011111111111) >>> 0 === 0b00000000000000000000000000101010) {
-        return `slt $${d} $${s} $${t}`
+        // return `slt $${d} $${s} $${t}`
     }
     else if ((instruction & 0b11111100000000000000011111111111) >>> 0 === 0b00000000000000000000000000101011) {
-        return `sltu $${d} $${s} $${t}`
+        // return `sltu $${d} $${s} $${t}`
     }
     else if ((instruction & 0b11111100000000000000000000000000) >>> 0 === 0b00010000000000000000000000000000) {
-        return `beq $${s}} $${t} ${i}`
+        // return `beq $${s}} $${t} ${i}`
     }
     else if ((instruction & 0b11111100000000000000000000000000) >>> 0 === 0b00010100000000000000000000000000) {
-        return `bne $${s}} $${t} ${i}`
+        // return `bne $${s}} $${t} ${i}`
     }
     else if ((instruction & 0b11111100000111111111111111111111) >>> 0 === 0b00000000000000000000000000001000) {
-        return `jr $${s}}`
+        // return `jr $${s}}`
     }
     else if ((instruction & 0b11111100000111111111111111111111) >>> 0 === 0b00000000000000000000000000001001) {
-        return `jalr $${s}}`
+        // return `jalr $${s}}`
     }
     else {
-        return "unknown instruction";
+        console.log("unknown instruction");
+        return state;
     }
 
 }
 
-let decode_file = function (filename:string) {
+let execute_file = function (filename: string) {
     fs.readFile(filename, 'utf-8', function (err, res) {
         let instructions: string[] = res.split(', ').map(function (value) {
             return value.replace(/\n/g, '').replace(/\r/g, '');
         });
         var prev_is_lis = false;
-        for (var i = 0; i < instructions.length; i++) {
-            let instruction = instructions[i];
-            let instruction_str: string = decode(parseInt(instruction, 2), prev_is_lis);
-            if (instruction_str.indexOf("lis") === 0) {
-                prev_is_lis = true;
-            } else {
-                prev_is_lis = false;
-            }
-            console.log(instruction_str);
-        }
+        
     });
 }
 
-decode_file('mips.mc');
+execute_file('mips.mc');
 
 // console.log('﻿00000000000000000010000000010100'.length);
 
